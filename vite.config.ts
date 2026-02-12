5372cf7d2caa96d2412ed8802d84ad12a472d083
LOLO <lolo.tw@gmail.com>
2026-02-12 15:13:59 +0800

    delete

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/network-dept-portal/', // 這裡要跟你 GitHub 上的 Repository 名稱一模一樣
+  base: '/network-dept-portal/', // 👈 這裡請確保前後都有斜線，且完全對應你的 GitHub 倉庫名
 })
