128569647914cacd6116b472076da8ac0135d416
LOLO <lolo.tw@gmail.com>
2026-02-24 16:59:00 +0800

    修改首頁效果

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/network-dept-portal/', // 👈 這裡請確保前後都有斜線，且完全對應你的 GitHub 倉庫名
+  base: './', // 👈 使用「點斜線」是解決 404 與空白頁的終極武器
 })
